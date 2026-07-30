7c77895e485d6f5562946a6318de4d758d97bdf9
AnahiSU <anahi7776@gmail.com>
2026-07-30 12:03:51 -0400

    fix: adjust layout structure in AppLayout component

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -4,14 +4,13 @@ import Navbar from "./Navbar";
 
 function AppLayout() {
     return (
-        <div className="flex h-screen bg-slate-50">
-            <Sidebar />
-            <div className="flex flex-col flex-1 overflow-hidden">
-                <Navbar />
+        <div className="flex flex-col h-screen bg-slate-50">
+            <Navbar />
+            <div className="flex flex-1 overflow-hidden">
+                <Sidebar />
                 <main className="flex-1 overflow-y-auto">
                     <Outlet />
                 </main>
-
             </div>
         </div>
     );
